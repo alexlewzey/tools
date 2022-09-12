034dac5075a68401ecdd472f8df89d6b76e31363
Alexander Lewzey <alexander.lewzey@netacea.com>
2022-09-12 09:24:31 +0100

    bug: text to speech hanging on second useage

--- clmac/macro/macros/text2speech.py
+++ clmac/macro/macros/text2speech.py
@@ -1,12 +1,19 @@
 import pyperclip
 import pyttsx3
+from multiprocessing import Process
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate', 250)
 
-def text2speech() -> None:
+
+def speak_clipboard():
     text = pyperclip.paste()
     engine.say(text)
     engine.runAndWait()
+
+
+def text2speech() -> None:
+    process = Process(target=speak_clipboard)
+    process.start()
