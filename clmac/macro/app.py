e7700dde04789669f8b8648a931028d4edf2e9b9
Alexander Lewzey <a.lewzey@gmail.com>
2023-05-13 10:39:37 +0100

    test

--- clmac/macro/app.py
+++ clmac/macro/app.py
@@ -1,6 +1,4 @@
-"""All my main macro and commonly used python scripts are run using a keyboard
-listener that recognises sequential key strokes that typically start with a
-semi colon.
+"""All my main macro and commonly used python scripts are run using a keyboard listener that recognises sequential key strokes that typically start with a semi colon.
 
 note: do not call your module macro as that name is already taken in the path
 """
