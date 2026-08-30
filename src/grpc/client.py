85cb0f50d2e47ddacda54351e426f6d4c1680323
Alexander Lewzey <a.lewzey@gmail.com>
2026-08-30 11:36:01 +0100

    update

--- src/grpc/client.py
+++ src/grpc/client.py
@@ -19,14 +19,14 @@ import grpc
 
 def predict_confidence(stub, features):
     request = inference_pb2.InferenceRequest(features=features)
-    response = stub.PredictConfidence(request)
+    response = stub.PredictConfidence(request, timeout=2)
     print(f"class_id: {response.class_id}")
     print(f"confidence: {response.confidence}")
 
 
 def predict_embedding(stub, text):
     request = inference_pb2.EmbeddingRequest(text=text)
-    response = stub.PredictEmbedding(request)
+    response = stub.PredictEmbedding(request, timeout=2)
     print(f"shape: {response.shape}")
 
     array = np.frombuffer(response.embedding, dtype=np.float16)
